55da8f2a31103d3bd540324395baa7b81bebf090
Jan Noha <nohajc@gmail.com>
2023-08-06 14:08:36 +0200

    wgnt-client (wip)

--- cmd/wgnt-client/main.go
+++ cmd/wgnt-client/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/nohajc/wg-nat-traversal/common/utils"
 )
@@ -22,7 +28,77 @@ func newConn() (*net.UDPConn, error) {
 	return conn, nil
 }
 
+type Client struct {
+	ServerURL string
+}
+
+func NewClient(serverHost string) *Client {
+	return &Client{
+		ServerURL: fmt.Sprintf("http://%s:8080/", serverHost),
+	}
+}
+
+func (c *Client) PublishPeerInfo(info *utils.STUNInfo) error {
+	reqPayload, err := json.Marshal(info)
+	if err != nil {
+		return err
+	}
+	resp, err := http.Post(c.ServerURL, "application/json", bytes.NewReader(reqPayload))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected http status: %s", resp.Status)
+	}
+	return nil
+}
+
+func (c *Client) GetPeerInfo(peerHost string) (*utils.STUNInfo, error) {
+	resp, err := http.Get(fmt.Sprintf("%s?ip=%s", c.ServerURL, peerHost))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNoContent {
+		return nil, nil
+	}
+	result := &utils.STUNInfo{}
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(result)
+	return result, err
+}
+
+func (c *Client) WaitForPeerInfo(peerHost string) (*utils.STUNInfo, error) {
+	for {
+		result, err := c.GetPeerInfo(peerHost)
+		if err != nil {
+			return nil, err
+		}
+		if result != nil {
+			return result, nil
+		}
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
 func main() {
+	var peerHost, serverHost string
+	flag.StringVar(&peerHost, "p", "", "peer IP/hostname")
+	flag.StringVar(&serverHost, "s", "", "server IP/hostname")
+	flag.Parse()
+
+	if peerHost == "" {
+		fmt.Fprintf(os.Stderr, "missing PEER_IP")
+		os.Exit(1)
+	}
+	if serverHost == "" {
+		fmt.Fprintf(os.Stderr, "missing SERVER_IP")
+		os.Exit(1)
+	}
+
 	conn, err := newConn()
 	if err != nil {
 		log.Fatalf("connection error: %v", err)
@@ -37,11 +113,19 @@ func main() {
 	fmt.Printf("NAT type: %s\n", stunInfo.NATKind)
 	if stunInfo.NATKind == utils.NAT_EASY {
 		fmt.Printf("%s -> %s:%d\n", conn.LocalAddr().String(), stunInfo.PublicIP, stunInfo.PublicPort)
+	} else {
+		fmt.Printf("%s -> %s:?\n", conn.LocalAddr().String(), stunInfo.PublicIP)
 	}
 
-	// ip, port, err := utils.GetPublicAddr(conn)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("%s:%d\n", ip, port)
+	client := NewClient(serverHost)
+	err = client.PublishPeerInfo(stunInfo)
+	if err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
+
+	peerInfo, err := client.WaitForPeerInfo(peerHost)
+	if err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
+	fmt.Printf("%s:%d - NAT: %s\n", peerInfo.PublicIP, peerInfo.PublicPort, peerInfo.NATKind)
 }
