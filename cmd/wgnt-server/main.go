0a014ed97c645b37ea2cacad59766e20a5f73d04
nohajc <nohajc@gmail.com>
2023-08-19 20:06:08 +0200

    wgnt-server: websockets (wip)

--- cmd/wgnt-server/main.go
+++ cmd/wgnt-server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -8,6 +9,8 @@ import (
 	"time"
 
 	"github.com/nohajc/wg-nat-traversal/common/nat"
+
+	"github.com/gorilla/websocket"
 )
 
 type Entry struct {
@@ -15,10 +18,175 @@ type Entry struct {
 	Expiry *time.Timer
 }
 
+var upgrader = websocket.Upgrader{}
+
 var peerTable = map[string]*Entry{}
 var peerTableMu sync.Mutex
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
+var pongWait = 30 * time.Second
+var pingInterval = pongWait * 2 / 3
+
+type WebSockRouter struct {
+	clients   map[string]*Client
+	clientsMu sync.RWMutex
+}
+
+func NewWebSockRouter() *WebSockRouter {
+	return &WebSockRouter{
+		clients: map[string]*Client{},
+	}
+}
+
+func (wsr *WebSockRouter) AddClient(pubKey string, c *Client) {
+	wsr.clientsMu.Lock()
+	wsr.clients[pubKey] = c
+	wsr.clientsMu.Unlock()
+}
+
+func (wsr *WebSockRouter) RemoveClient(pubKey string) {
+	wsr.clientsMu.Lock()
+	defer wsr.clientsMu.Unlock()
+
+	c, ok := wsr.clients[pubKey]
+	if !ok {
+		return
+	}
+
+	close(c.writeChan)
+	if err := c.conn.Close(); err != nil {
+		log.Printf("error closing socket: %v", err)
+	}
+
+	delete(wsr.clients, pubKey)
+}
+
+type Client struct {
+	pubKey    string
+	conn      *websocket.Conn
+	router    *WebSockRouter
+	writeChan chan WriteRequest
+}
+
+func NewClient(pubKey string, conn *websocket.Conn, router *WebSockRouter) *Client {
+	return &Client{
+		pubKey:    pubKey,
+		conn:      conn,
+		router:    router,
+		writeChan: make(chan WriteRequest, 4096),
+	}
+}
+
+type WriteRequest struct {
+	message    Message
+	statusChan chan error
+}
+
+func MakeWriteRequest(msg Message) WriteRequest {
+	return WriteRequest{
+		message:    msg,
+		statusChan: make(chan error, 1),
+	}
+}
+
+func (r *WriteRequest) Error() chan error {
+	return r.statusChan
+}
+
+type Message struct {
+	// TODO
+}
+
+func (m *Message) String() string {
+	return "" // TODO
+}
+
+func (c *Client) readMessage() (Message, error) {
+	msg := Message{}
+	err := c.conn.ReadJSON(&msg)
+	return msg, err
+}
+
+func (c *Client) readIncoming() {
+	defer c.router.RemoveClient(c.pubKey)
+
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(appData string) error {
+		log.Println("pong")
+		return c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	})
+
+	for {
+		msg, err := c.readMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("socket read error: %v", err)
+			}
+			break
+		}
+		log.Printf("Message: %s", &msg)
+	}
+}
+
+func (c *Client) writeMessage(ctx context.Context, msg Message) error {
+	req := MakeWriteRequest(msg)
+	c.writeChan <- req
+
+	select {
+	case err := <-req.Error():
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func (c *Client) writeOutgoing() {
+	ticker := time.NewTicker(pingInterval)
+	defer func() {
+		ticker.Stop()
+		c.router.RemoveClient(c.pubKey)
+	}()
+
+	for {
+		select {
+		case wReq, ok := <-c.writeChan:
+			if !ok {
+				_ = c.conn.WriteMessage(websocket.CloseMessage, nil)
+				return
+			}
+			err := c.conn.WriteJSON(&wReq.message)
+			wReq.statusChan <- err
+
+		case <-ticker.C:
+			log.Println("ping")
+			err := c.conn.WriteMessage(websocket.PingMessage, nil)
+			if err != nil {
+				log.Printf("socket write error: %v", err)
+				return
+			}
+		}
+	}
+}
+
+func (wsr *WebSockRouter) wsRequestHandler(w http.ResponseWriter, r *http.Request) {
+	pubKey := r.URL.Query().Get("pubkey")
+	if pubKey == "" {
+		st := http.StatusBadRequest
+		http.Error(w, http.StatusText(st), st)
+		return
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("socket upgrade failed: %v", err)
+		return
+	}
+
+	c := NewClient(pubKey, conn, wsr)
+	go c.readIncoming()
+	go c.writeOutgoing()
+}
+
+func (wsr *WebSockRouter) requestHandler(w http.ResponseWriter, r *http.Request) {
 	pubKey := r.URL.Query().Get("pubkey")
 	if pubKey == "" {
 		st := http.StatusBadRequest
@@ -44,6 +212,17 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 		} else {
+			wsPeer, ok := wsr.clients[pubKey]
+			if ok {
+				err := wsPeer.writeMessage(r.Context(), Message{})
+				if err == nil {
+					// TODO: wait for response from peer
+					// which will announce the port mapping
+					log.Printf("notified peer %s", pubKey)
+				} else {
+					log.Printf("failed to notify peer %s: %v", pubKey, err)
+				}
+			}
 			w.WriteHeader(http.StatusNoContent)
 		}
 	case http.MethodPost:
@@ -82,6 +261,8 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", requestHandler)
+	wsr := NewWebSockRouter()
+	http.HandleFunc("/", wsr.requestHandler)
+	http.HandleFunc("/ws", wsr.wsRequestHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go.mod
+++ go.mod
@@ -3,30 +3,26 @@ module github.com/nohajc/wg-nat-traversal
 go 1.20
 
 require (
-	github.com/stretchr/testify v1.8.4
+	github.com/pion/transport/v2 v2.2.1
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20230429144221-925a1e7659e6
 )
 
 require (
 	github.com/pion/dtls/v2 v2.2.7 // indirect
 	github.com/pion/logging v0.2.2 // indirect
-	github.com/pion/transport/v2 v2.2.1 // indirect
 )
 
 require (
-	github.com/ccding/go-stun/stun v0.0.0-20200514191101-4dc67bcdb029
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/gorilla/websocket v1.5.0
 	github.com/josharian/native v1.1.0 // indirect
 	github.com/mdlayher/genetlink v1.3.2 // indirect
 	github.com/mdlayher/netlink v1.7.2 // indirect
 	github.com/mdlayher/socket v0.4.1 // indirect
 	github.com/pion/stun v0.6.1
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.zx2c4.com/wireguard v0.0.0-20230325221338-052af4a8072b // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
